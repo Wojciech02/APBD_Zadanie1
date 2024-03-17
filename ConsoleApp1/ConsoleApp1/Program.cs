f242bb36a8871a9dba13e16e1c773fd0543727ca
s24618 <s24618@pjwstk.edu.pl>
2024-03-17 12:08:01 +0100

    Max1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,3 +8,16 @@
     float res = sum / tab.Length;
     return res;
 } 
+
+static int max(int[] tab)
+{
+    int max = tab[0];
+    for(int i =1;i<tab.Length;i++)
+    {
+        if(max < tab[i])
+        {
+            max = tab[i];
+        }
+    }
+    return max;
+}
