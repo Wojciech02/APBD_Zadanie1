d0515525d97110be865b059ae394f07480b05435
s24618 <s24618@pjwstk.edu.pl>
2024-03-17 11:57:41 +0100

    Implementacja 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿static float avrage(int[] tab)
+{
+    int sum = 0;
+    for(int i=0; i < tab.Length; i++)
+    {
+        sum += tab[i];
+    }
+    float res = sum / tab.Length;
+    return res;
+} 
